b62f06d0df7ffbff939ba7d75d716ac831027e93
sow221 <sowmjr21@gmail.com>
2026-05-14 23:04:27 +0000

    fix: remove invite codes - fix statsService route

--- src/services/statsService.js
+++ src/services/statsService.js
@@ -2,8 +2,7 @@ import api from './api';
 
 export const statsService = {
     getDashboardStats: async () => {
-        const response = await api.get('/admin/dashboard-stats');
-        return response.data; 
-        // Doit retourner { totalElecteurs, votesClotures, votesEnCours, participationRate, recentesElections: [] }
+        const response = await api.get('/admin/stats-globales');
+        return response.data;
     }
 };
